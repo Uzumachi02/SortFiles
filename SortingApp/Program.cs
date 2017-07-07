7b290472ed26c27e85e15a342cac713ba92bd784
Uzumachi <uzumachi02@gmail.com>
2017-07-07 16:10:43 +0300

    init

---
+++ SortingApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SortingApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.OutputEncoding = Encoding.UTF8;
+      Console.WriteLine("Hello Uzu");
+      //List<string> files = Directory.GetFiles(@"C:\Users\Uzumachi\Downloads").ToList();
+      //List<string> dirs = Directory.GetDirectories(@"C:\Users\Uzumachi\Downloads").ToList();
+      var app = new SortApp(@"C:\Users\Uzumachi\Downloads");
+
+      Console.WriteLine("Path: {0}", app.Path);
+      foreach( string dir in app.Dirs ) {
+        Console.WriteLine(dir);
+      }
+      app.RunSorter();
+      //Directory.CreateDirectory(@"C:\Users\Uzumachi\Downloads\.xls");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ SortingApp/SortApp.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SortingApp
+{
+  class SortApp
+  {
+    public List<string> Dirs { get; set; }
+    public List<string> Files { get; set; }
+    public string Path { get; set; }
+
+    public SortApp()
+    {
+      Init();
+    }
+
+    public SortApp(string path)
+    {
+      this.Path = path;
+      Init();
+    }
+
+    public void Init() {
+      Files = GetFiles();
+      Dirs = GetDirs();
+    }
+
+    public string GetCurPath()
+    {
+      if( string.IsNullOrWhiteSpace(Path) )
+        Path = Directory.GetCurrentDirectory();
+
+      return Path;
+    }
+
+    public List<string> GetDirs() {
+      return Directory.GetDirectories(GetCurPath()).ToList();
+    }
+    public List<string> GetFiles() {
+      return Directory.GetFiles(GetCurPath()).ToList();
+    }
+
+    public void RunSorter()
+    {
+      foreach( string file in Files ) {
+        string fileName = System.IO.Path.GetFileName(file);
+        string fileExt = System.IO.Path.GetExtension(fileName);
+
+        Console.WriteLine("{0} - {1}", fileName, fileExt);
+        string pathToMove = GetDirPathToMove(fileExt);
+        string pathSource = System.IO.Path.Combine(pathToMove, fileName);
+        pathSource = CheckSourcePath(pathSource);
+        Console.WriteLine("{0} was moved to {1}.", file, pathSource);
+        File.Move(file, pathSource);
+      }
+    }
+
+    private string GetDirPathToMove(string dirName)
+    {
+      string dirPath = System.IO.Path.Combine(Path, dirName);
+      if( !Directory.Exists(dirPath) ) {
+        Directory.CreateDirectory(dirPath);
+      }
+
+      return dirPath;
+    }
+
+    private string CheckSourcePath(string srcPath, int recurs = 1) {
+      if( File.Exists(srcPath) ) {
+        string dirName = System.IO.Path.GetDirectoryName(srcPath);
+        string fileName = System.IO.Path.GetFileNameWithoutExtension(srcPath);
+        string fileExt = System.IO.Path.GetExtension(srcPath);
+
+        string newNameFile = string.Format("{0} (d-{1}){2}", fileName, recurs++, fileExt);
+        srcPath = System.IO.Path.Combine(dirName, newNameFile);
+        return CheckSourcePath(srcPath, recurs);
+      }
+
+      return srcPath;
+    } 
+  }
+}
